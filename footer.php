ef6c348caa07bce208614296d0396884fd24f2ed
rodrigocaballero <rodrigo.cl@outlook.com>
2015-02-10 20:15:25 -0300

    Revert "footer, header"
    This reverts commit c484abb65ed50e0ae6069432f8d547eac37dde58.

--- footer.php
+++ footer.php
@@ -1,23 +1,6 @@
 <?php include_once "header.php"; ?>
 <footer class="footer">
-    <div class="container">
-        <br>
-        <img src="img/logo_blanco.png">
-        <br>
-        <br>
-        <p>Las ilustraciones utilizadas en este sitio NO son propiedad de "ElMazo.cl", estas pertenecen a "Klu" y "FENIX ENTERTAINMENT".-</p>
-        <p>Por favor no nos demanden.-</p>
-        <p>ElMazo.cl no se hace responsable por rabietas de tus amigos (o tuyas), al destruir su estrategia (o al ser destruido).-</p>
-        <p>ElMazo.cl no se asegura que le vas a ganar a tus amigos.-</p>
-        <br>
-        <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" /></a>
-    
-
-    </div>
-          <div id="links_rrss">
-        <u>
-            <li><a href="https://www.facebook.com/elmazocl" target="_blank"><i class="fa fa-facebook-official fa-3x"></i></a></li>
-            <li><a href="https://twitter.com/elmazocl" target="_blank"><i class="fa fa-twitter fa-3x"></i></a></li>
-        </u>
-    </div>  
-</footer>
+      <div class="container">
+        <img src="img/logo_gris.png">
+      </div>
+    </footer>
