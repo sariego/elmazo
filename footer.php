7a01805c3e67dca15c32134f9f4f1207596d4197
rodrigocaballero <rodrigo.cl@outlook.com>
2015-02-11 16:10:49 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,6 +1,23 @@
 <?php include_once "header.php"; ?>
 <footer class="footer">
-      <div class="container">
-        <img src="img/logo_gris.png">
-      </div>
-    </footer>
+    <div class="container">
+        <br>
+        <img src="img/logo_blanco.png">
+        <br>
+        <br>
+        <p>ElMazo.cl no tiene derechos sobre las ilustraciones utilizadas en este sitio, estas son propias de "Mitos y Leyandas" y "klu".-</p>
+        <p>Por favor no nos demanden.-</p>
+        <p>ElMazo.cl no te asegura victorias contra tus amigos, así que no nos reclamen.-</p>
+        <p>Agradecimientos a <a href="#">ElReino.cl</a></p>
+    </div>
+    <div id="rrss">
+        <ul>
+            <li><a href="https://facebook.com/elmazocl" target="_blank"><i class="fa fa-facebook-official fa-3x"></i></a></li>
+            <li><a href="https://twitter.com/elmazocl" target="_blank"><i class="fa fa-twitter fa-3x"></i></a></li>
+        </ul>
+    </div>
+    <div id="info_footer">        
+        <p>Para mas información y soporte: contacto@elmazo.cl</p>
+        <p>Sitio desarrollado por ETP.-</p>
+    </div>
+</footer>
